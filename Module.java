8a1ceafba8059842954199d343290824e16a018b
AbdelrahmanAbdelhalim <56049226+AbdelrahmanAbdelhalim@users.noreply.github.com>
2020-04-16 06:43:17 +0100

    Students can enroll now

--- Module.java
+++ Module.java
@@ -1,34 +1,52 @@
 import java.util.ArrayList;
 public class Module{
-	String name;
-	int capacity;
-	String moduleCode;
-	String moduleCoordinator;
-	ArrayList<Student> currentStudents = new ArrayList<Student>(); // to prevent the same student from enrolling twice
-	ArrayList<Student> waitingList = new ArrayList<Student>();
+	private String name;
+	private int capacity;
+	private String moduleCode;
+	private String moduleCoordinator;
+	private ArrayList<Student> currentStudents = new ArrayList<Student>(); // to prevent the same student from enrolling twice
+	private ArrayList<Student> waitingList = new ArrayList<Student>();
 
 	/*
 	total number of students ina module can not exceed its capacity
 	passive process
 	*/
-	public boolean alreadyEnrolled(Student student){
+	public Module (String name, String moduleCode, String moduleCoordinator, int capacity){
+		this.name = name;
+		this.moduleCode = moduleCode;
+		this.moduleCoordinator = moduleCoordinator;
+		this.capacity = capacity;
+	}
+	public  boolean alreadyEnrolled(Student student){
 		for(Student s : currentStudents){
 			if(student.equals(s)){
-				System.out.println("Studnt("student.getStudentId()") is already enrolled")
+				String studentId = student.getStudentId();
+				System.out.println("Studnt("+ studentId + ") is already enrolled");
 				return true;
 			}
 		}
 		return false;
 	}
-	public boolean canEnroll (){
-		return currentStudents.size() < capacity;
+	public  boolean canEnroll (Student student){
+		if(currentStudents.size() < capacity){
+			return true;
+		}
+		System.out.println("Module " + name + " is full at the moment");
+		waitingList.add(student);
+		return false;
 	}
-	public void enroll(Student student){
-		if(!alreadyEnrolled(student) && canEnroll()){
-			currentStudents.add(student);
-		}	
+	public  void addStudent(Student student){
+		currentStudents.add(student);
 	}
 
+	public void printEnrolledStudents(){
+		for(Student st : currentStudents){
+			System.out.println(st.getStudentId());
+		}
+	}
+	public String getModuleId(){
+		return moduleCode;
+	}
 	public boolean equals(Module module){
 		if(this.moduleCode == module.moduleCode){
 			return true;
--- Student.java
+++ Student.java
@@ -1,12 +1,53 @@
 import java.util.ArrayList;
 public class Student implements Runnable{
-	String name;
-	String studentID;
-	int numOfModulesEnrolled;
-	ArrayList<Module> Modules = new ArrayList<>();
+	private String name;
+	private String studentID;
+	private int numOfModulesEnrolled;
+	private ArrayList<Module> modules = new ArrayList<>();
+	ArrayList<Module> modulesToJoin = new ArrayList<>(); //this arraylist is used for purposes of testing
+	private boolean online = true;
+	
+
+	public Student(String name, String studentID){
+		this.name = name;
+		this.studentID = studentID;
+	}
+	public void enroll(Module module){
+		try{
+			if(!module.alreadyEnrolled(this) && module.canEnroll(this)){
+			module.addStudent(this);
+			this.notify();
+			modules.add(module);
+			System.out.println("Student " + name + " Successfully Enrolled in module: " + module.getModuleId());
+			}else{
+				// while(!module.canEnroll(this)){
+				// 	try{
+				// 		this.wait();
+				// 	}catch(Exception ex){
+				// 		System.out.println(ex + "student");
+				// 	}
+				// }
+			}
+		}catch(Exception ex){
+			System.out.println(ex + studentID);
+		}
+	}
+	public void withdraw(Module module){
+		for(Module md : modules){
+			if(module.equals(md)){
+
+			}
+		}
+	}
+
+	public String getStudentId(){
+		return studentID;
+	}
 
 	public void run(){
-		
+		for(Module module: modulesToJoin){
+			enroll(module);
+		}
 	}
 }
 /*
